65e16de0398f1a3a9a6c28b63aea17a441eb6f2a
erioslino <eduardo.rios.lino@gmail.com>
2026-07-26 17:18:39 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const cors = require('cors');
 
 const app = express();
-const port = 3306;
+const port = 57431;
 
 // Middlewares: Permitir conexión desde React y parsear JSON
 app.use(cors());
