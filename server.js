198d0cac6eff98d86139019c6f2b6c9e0260120f
erioslino <eduardo.rios.lino@gmail.com>
2026-07-27 20:31:16 -0400

    commit for Reilway

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost'
   user: process.env.DB_USER || 'root'
-  password: process.env.DB_PASSWORD || ''
+  password: process.env.DB_PASSWORD || 'dlgGkasTifnllFcSDXEqSzRgULbeCqfN'
   database: process.env.DB_NAME || 'railway'
   port: process.env.DB_PORT || 3306
 })
